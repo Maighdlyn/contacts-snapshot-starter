f0cdef1f05e217524ff0d93f42ef2b8bcf342d3f
Ryan Kent <imsolost@gmail.com>
2017-08-01 09:27:44 -0700

    got incorrect password warning working

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -4,17 +4,17 @@ const {renderError} = require('../utils')
 const router = require('express').Router()
 
 router.get('/login', (request, response) => {
-  response.render('login')
+  response.render('login', {warning: ''})
 })
 
 router.post('/login', (request, response) => {
   dbUsers.getPassword(request.body.username)
     .then(function(user) {
-      alert("Alert!")
+      // alert("Alert!")
       if (user.password === request.body.password) {
         response.redirect(`/`)
       }
-      // else alert("Alert!")
+      else response.render('login', {warning: 'Incorrect password'})
 
     })
     .catch( error => renderError(error, response, response) )
