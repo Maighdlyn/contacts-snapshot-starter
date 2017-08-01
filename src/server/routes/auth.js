9a04259a8be0e71fa7dc69e3c65f52bf5b7d01e8
Ryan Kent <imsolost@gmail.com>
2017-08-01 09:42:29 -0700

    added warning and formatted pages

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -10,11 +10,26 @@ router.get('/login', (request, response) => {
 router.post('/login', (request, response) => {
   dbUsers.getPassword(request.body.username)
     .then(function(user) {
-      // alert("Alert!")
       if (user.password === request.body.password) {
         response.redirect(`/`)
       }
       else response.render('login', {warning: 'Incorrect password'})
+
+    })
+    .catch( error => renderError(error, response, response) )
+})
+
+router.get('/signup', (request, response) => {
+  response.render('signup', {warning: ''})
+})
+
+router.post('/signup', (request, response) => {
+  dbUsers.getPassword(request.body.username)
+    .then(function(user) {
+      if (user.password === request.body.password) {
+        response.redirect(`/`)
+      }
+      else response.render('signup', {warning: 'Incorrect password'})
 
     })
     .catch( error => renderError(error, response, response) )
