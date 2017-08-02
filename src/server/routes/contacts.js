54ac957f3e20e0fe458c21427295b599f773252f
maighdlyn <emmamaighdlyn@gmail.com>
2017-08-02 13:28:34 -0700

    Contact view only shows delete button when user is admin

--- src/server/routes/contacts.js
+++ src/server/routes/contacts.js
@@ -5,40 +5,19 @@ const {renderError} = require('../utils')
 
 const router = require('express').Router()
 
-// router.get('/new', (request, response) => {
-//   response.render('new')
-// })
-
-// router.post('/', (request, response, next) => {
-//   dbContacts.createContact(request.body)
-//     .then(function(contact) {
-//       if (contact) return response.redirect(`/contacts/${contact[0].id}`)
-//       next()
-//     })
-//     .catch( error => renderError(error, response, response) )
-// })
-
 router.get('/:contactId', (request, response, next) => {
   const contactId = request.params.contactId
   if (!contactId || !/^\d+$/.test(contactId)) return next()
   dbContacts.getContact(contactId)
     .then(function(contact) {
-      if (contact) return response.render('show', { contact })
+      if (contact) return response.render('show', {
+        contact,
+        role: request.user.role
+       })
       next()
     })
     .catch( error => renderError(error, response, response) )
 })
-
-
-// router.get('/:contactId/delete', (request, response, next) => {
-//   const contactId = request.params.contactId
-//   dbContacts.deleteContact(contactId)
-//     .then(function(contact) {
-//       if (contact) return response.redirect('/')
-//       next()
-//     })
-//     .catch( error => renderError(error, response, response) )
-// })
 
 router.get('/search', (request, response, next) => {
   const query = request.query.q
