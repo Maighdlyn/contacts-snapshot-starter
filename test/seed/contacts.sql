1113a2adcf51301c08f7365c9fc7fff692566551
maighdlyn <emmamaighdlyn@gmail.com>
2017-08-02 17:41:21 -0700

    Update users seed data with bcrypt passwords

--- test/seed/contacts.sql
+++ test/seed/contacts.sql
@@ -1,3 +1,7 @@
-INSERT INTO users (username, password) VALUES ('Daenerys', 'dragons'), ('Rhaegar', 'promisedprince'), ('Aerys', 'burnthemall');
+INSERT INTO users (username, password, role) VALUES ('Daenerys', '$2a$11$EMDyHLnN.rsgPHmZ/ZP8TusQWNu6LlrYkrvar4Z6fbrnzMlsieE0m', 'admin'), ('Rhaegar', '$2a$11$Vv1x7Qgf6haqOrCeH2m6qedbbn9uygyHhoBZoMfddb3ocJMK4CQtC', 'regular'), ('Aerys', '$2a$11$gxefuJaCWJE8HepXCXZwAebkdVrgEUgnlpN8BODw6oObd5PVsQv1K', 'regular');
+
+-- Daenerys's password = dragons
+-- Rhaegar's password = promisedprince
+-- Aerys's password = burnthemall
 
 INSERT INTO contacts (first_name, last_name) VALUES ('Arya', 'Stark'), ('Bran', 'Stark'), ('Sansa', 'Stark'), ('Jon', 'Snow');
